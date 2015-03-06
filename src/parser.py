bbb0a8686af936e89c2d4da7a3f84f21435624a7
ashah <ashah@px.ftw>
2015-03-05 16:04:05 -0800

    traffic bias weights generator and used in parser day_summarization

--- src/parser.py
+++ src/parser.py
@@ -20,7 +20,7 @@ years=[yr for yr in os.listdir(os.path.join(datadir,airport)) \
 
 
 
-def day_summarization(filename=None):
+def day_summarization(filename,weights):
 	"""Take a csv file of METAR data for a day at single site
 	and parse appropriate columns and perform summarization
 
@@ -36,10 +36,15 @@ def day_summarization(filename=None):
 	
 	#drop the following columns
 	timeLabel = df.columns.values[0] #fluctuates from 'TimeEST' to 'TimeEDT'
+	"""
 	dropLabels = ['FullMetar','DateUTC<br />', timeLabel, \
 	'Wind Direction','Gust SpeedMPH','Events','Conditions', \
 	'WindDirDegrees', 'Sea Level PressureIn', 'Dew PointF']
-	
+	"""
+	dropLabels = ['FullMetar', 'DateUTC<br />', \
+	'Wind Direction','Gust SpeedMPH','Events','Conditions', \
+	'WindDirDegrees', 'Sea Level PressureIn', 'Dew PointF']
+
 	df.drop(labels=dropLabels,axis=1,inplace=True)
 	
 	#convert non-numeric entries like 'Calm' in Windspeed to NaN
@@ -51,9 +56,29 @@ def day_summarization(filename=None):
 	#weights determined by time period of interest 
 	#e.g. if interested for 1000, weight +/- 2hr these unity
 	#less weight for other times before after
+	if not weights:
+		weights=np.ones(24)
 
-	#for now pick noon time
-	return df.iloc[11,:]
+	#METAR can have more than 24 obs (multiple obs per hour)
+	#Sow we'll create another column called Hour to weight it
+	#properly with traffic biasing
+	#The hour entry in the Hour column will allow us to assign
+	#the same weight for multiple obs in the same hour
+	df['Hour']=[a.hour for a in pd.to_datetime(df[timeLabel])]
+	df['weights']=[weights[a] for a in df['Hour']]
+
+
+	#now return the weighted average
+	wdf=pd.DataFrame()
+	#need to drop time string column before arithmetic
+	df.drop(labels=timeLabel,axis=1,inplace=True)
+
+	for a in df.columns.values:
+		wdf[a] = [df[a].dot(df.weights).sum()/df.weights.sum()]
+
+	wdf.drop(labels=['Hour','weights'],axis=1,inplace=True)
+	#return weighted average
+	return wdf
 
 #create the data matrix
 data = list()
---
+++ src/traffic_bias.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+  Traffic bias weights generator
+  ~~~~~~~~~~~~~~
+
+  Generates weights for each hour from ASPM traffic data. 
+  Our dataset encompasses daily scheduled departure and arrival
+  between 2010-2013.  Assumes data is in csv format and parsed with
+  Kenny's parser.
+
+  Two main functions:
+  1. generate_average_hourly_traffic -
+
+  @param: string for airport code
+  @returns: pandas data frame of hourly mean depatures and arrivals
+
+  2. get_traffic_bias_weights - 
+
+  @param: pandas data frame of hourly mean departures and arrivals
+  @returns: normalized weights
+
+
+
+
+
+  :copyright: (c) 2015 by Akhil Shah, Kenneth Kuhn, RAND Corp.
+  
+"""
+import pandas as pd
+import glob
+import os
+
+def generate_average_hourly_traffic(airport_code = 'JFK', data_dir = None):
+	if not data_dir:
+		datadir='/Users/ashah/NoBackup/code/nasa/data/ASPM_CSV'
+	filelist = glob.glob(os.path.join(datadir,'*.csv'))
+	trafficDF = pd.DataFrame()
+	for fn in filelist:
+		df = pd.read_csv(fn)
+		#csv files have a whitespace before airport code in Facility 
+		dfJ = df[df.Facility == ' '+ airport_code].loc[:,['Date','Hour','ScheduledDepartures','ScheduledArrivals']]
+		dfJts=dfJ.set_index(['Date','Hour'])
+		dfJts.columns=['D','A']
+		trafficDF = trafficDF.append(dfJts)
+
+	avg_traffic = trafficDF.groupby(level='Hour').mean()
+	return avg_traffic
+
+
+def traffic_bias_weights(frame):
+	weights = (frame.D + frame.A)/(frame.D.sum() + frame.A.sum())
+	return weights.as_matrix()
+
+
+
+
+
+
+
+
