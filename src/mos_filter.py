f1eda5ef074e085d82c169bb93904412ff3871e3
ashah <ashah@px.ftw>
2015-05-29 17:18:22 -0700

    mos feature extractor

--- src/mos_filter.py
+++ src/mos_filter.py
@@ -42,15 +42,54 @@ df_filtered=df[df.fotime-df.rtime <= pd.Timedelta('9 hours')]
 
 #----DEPRECATE ABOVE----
 
+#now add forecast date and hour columns
+df_filtered['fdate']=[a.date() for a in df_filtered.fotime]
+df_filtered['fhour']=[a.hour for a in df_filtered.fotime]
 
 #drop columns below keeping only numerical
-dropLabels = ['station', 'model', 'runtime', 'ftime', 'n_x',\
-'tmp', 'dpt', 'cld', 'wdr', 'snw', 'cig', 'vis', 'obv','typ']
+dropLabels = ['station', 'model', 'runtime', 'ftime',\
+'n_x', 'cld', 'snw', 'cig', 'obv','typ']
 
 df_filtered.drop(labels=dropLabels,axis=1,inplace=True)
 
+#drop further columns since they are all NA once we restrict to tighest horizon
+#verify this for each airport using: df_filtered.isnull().sum()
+dropNAcols= ['p06','p12','q06','q12','t06','t12']
+df_filtered.drop(labels=dropNAcols,axis=1,inplace=True)
+
+#keep only those features we'll cluster on (following METAR expert judgement)
+dropNonclust = ['tmp','dpt','wdr','rtime','fotime']
+df_filtered.drop(labels=dropNonclust,axis=1,inplace=True)
+
+#index the rows with the date
+df_filtered.set_index(keys='fdate',inplace=True,drop=True)
+
+#create a list of dataframes each belonging to a given hour
+hour_group=df_filtered.groupby('fhour')
+hours=pd.unique(df_filtered.fhour)
+dfflist=[hour_group.get_group(a) for a in hours]
+colnames=dfflist[0].columns.values.tolist()
+#name each of these different dataframes with column-names + hour
+cnamelist=[[c+str(h) for c in colnames] for h in hours]
+for i,a in enumerate(cnamelist): 
+    dfflist[i].columns=cnamelist[i]
+    dfflist[i].drop(cnamelist[i][-1],axis=1,inplace=True)
+
+
+
+#now join these
+dffj=pd.DataFrame()
+df_filtered_joined=dffj.join(dfflist)
+
 #save all of these features to csv - note there will be many NA's
 fn=open('./'+ airport +'_MOS_filtered_features.csv','w')
-df_filtered.to_csv(fn,index=False)
+df_filtered_joined.to_csv(fn,index=False)
 fn.close()
+
+#cluster
+datmat=df_filtered_joined.values
+#turn nan to 0
+datmat=np.nan_to_num(datmat)
+daymat=[a.strftime('%m/%d/%Y') for a in df_filtered_joined.index.get_values()]
+
 
